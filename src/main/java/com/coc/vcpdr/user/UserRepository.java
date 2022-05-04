0c1dca6257e03fb6e5ce554c2de5467195635300
Jacob <Hahkobeh@gmail.com>
2022-05-04 14:46:56 -0600

    added repository search function

--- src/main/java/com/coc/vcpdr/user/UserRepository.java
+++ src/main/java/com/coc/vcpdr/user/UserRepository.java
@@ -3,7 +3,9 @@ package com.coc.vcpdr.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+    List<User> findAllByPersonSurName(String personSurName);
 }
